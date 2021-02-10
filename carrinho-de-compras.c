4ee90dc94d3541cff11c5ba9935d15ce6a555232
Marcos Rocha <marcosrocha.br@hotmail.com>
2021-02-10 08:56:21 -0300

    Troca do WHILE por FOR e adicao de comentarios

--- carrinho-de-compras.c
+++ carrinho-de-compras.c
@@ -1,23 +1,27 @@
+//Agoritmo de um carrinho de compras que tem capacidade para guardar 200 ítens utilizando uma estrutura que possui:
+//NOME DO ITEM, PREÇO e QUANTIDADE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 typedef struct {
     char nome[50];
     float preco;
     int quant;
-} produto;
+} produto; //Estrutura 'produto'
 
 int main(){
 
-    produto p[200];
+    produto p[200]; //Cada ítem com a estrutura 'produto' é guardado dentro de um vetor p com 200 índices
     float total;
-    int x = 0;
     char sair;
+    int x;
 
     printf("\t\tCarrinho de compras\n\n");
 
-    do{
+    for( x = 0; x < 200; x++){
         printf("Nome: ");
         scanf(" %s", p[x].nome);
 
@@ -29,17 +33,21 @@ int main(){
 
         total = total + p[x].preco * p[x].quant;
 
-        printf("Deseja sair S/N? ");
-        scanf(" %c", &sair);
+		//Antes de ler a estrutura do próximo ítem, coloquei a opção de 
+		//poder interromper o laço e finalizar o programa, caso necessário
+		
+        printf("Deseja sair S/N? "); 
+        scanf(" %c", &sair);		
 
         printf("\n");
-
-        x++;
-
-    } while (sair != 's' && 'S');
+        
+        if(sair == 's' || sair == 'S')
+        	break;
+    } 
 
     printf("\n\t\tTotal = R$ %.2f", total);
 
+	printf("\n\n");
     system("pause");
     return 0;
 }
